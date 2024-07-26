935f729c35ae996c2da4a959717f92c73b06230a
Daniel-Mdz <danielrandiss@gmail.com>
2024-07-25 22:38:18 -0500

    Grupo_2: Practica Calificada 2 => Ejercicio 1

--- ejer_1.js
+++ ejer_1.js
@@ -5,3 +5,25 @@
 // devuelva la suma de todos los elementos del array. 
 // Llama a la función sumaArray pasando el array numeros y muestra el resultado 
 // en la consola. 
+
+console.log("-------------------------------------------------");
+console.log("---------- Array [1,2,3,4,6,7,8,9,10] -----------");
+console.log("----------------- Suma de todos -----------------");
+console.log("-------------------------------------------------");
+
+let numeros = [];
+for (let i=1; i<=10; i++) {
+    numeros.push(i); // Agrega numeros al array
+    console.log("Posicion "+(i-1)+": "+numeros[i - 1]); // Imprime cada número del array (el -1 idica la posicion )
+}
+// console.log(numeros);
+
+function sumaArray(array) {
+    let suma = 0;
+    for (let i=0; i<array.length; i++) { // recorre el numero de valores que existe en el array
+        suma += array[i];
+    }
+    return suma;
+}
+console.log("-------------------------------------------------");
+console.log("Suma de los números del array: "+sumaArray(numeros));
