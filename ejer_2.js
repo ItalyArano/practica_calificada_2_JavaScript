cc42876029840dc41e0f50c4f29138147a0d8276
Daniel-Mdz <danielrandiss@gmail.com>
2024-07-25 23:08:49 -0500

    sync

--- ejer_2.js
+++ ejer_2.js
@@ -5,3 +5,22 @@
 // devuelva un nuevo array solo con los números pares. 
 // Usa la función filtrarPares para filtrar los números pares del array numeros y 
 // muestra el nuevo array en la consola.
+
+function esPar(numero) {
+  return numero % 2 === 0; // compara y devuelve un valor booleano
+}
+
+function filtrarPares(numeros) {
+    let array = [];
+    for (let i = 0; i < numeros.length; i++) { // recorre desde el cero las veces que los valores existen
+        if (esPar(numeros[i])) {
+            array.push(numeros[i]);
+        }
+    }
+    return array;
+//   return numeros.filter(esPar);
+}
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log("Números pares:", filtrarPares(numeros)); // Muestra los números pares en la consola
