8163d28b1963b01f9077c35c5d0ff399bdb59324
JuanGeldres <geldresjuan36@gmail.com>
2024-07-25 22:53:25 -0500

    RAMAEJERCICIO

--- ejer_3.js
+++ ejer_3.js
@@ -5,3 +5,18 @@
 // condicional para verificar si el carácter es una vocal (a, e, i, o, u). 
 // Prueba la función contarVocales con la cadena "JavaScript es divertido" y 
 // muestra el resultado en la consola.
+
+function contarVocales(cadena) {
+    let vocales = ['a', 'e', 'i', 'o', 'u'];
+    let contador = 0;
+
+    for (let i = 0; i < cadena.length; i++) {
+        let caracter = cadena[i].toLowerCase();
+
+        if (vocales.includes(caracter)) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
